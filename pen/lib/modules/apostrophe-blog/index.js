50c9dc04984c1c6e05d0bd623ffe2ce68d2a80c4
JonasGruenwald <jonas.gruenwald@students.fh-hagenberg.at>
2018-01-18 18:42:45 +0100

    disqus basic

--- pen/lib/modules/apostrophe-blog/index.js
+++ pen/lib/modules/apostrophe-blog/index.js
@@ -5,23 +5,40 @@ module.exports = {
     alias: 'projects',
     label: 'Project',
 
-  contextual: true,
+    contextual: true,
 
-  addFields: [
-    {
-      name: '_author',
-      label: 'Author',
-      type: 'joinByOne',
-      withType: 'apostrophe-user',
-      idField: 'userId',
+    addFields: [
+        {
+            name: '_author',
+            label: 'Author',
+            type: 'joinByOne',
+            withType: 'apostrophe-user',
+            idField: 'userId',
+            contextual: true
+        },
+
+        {
+            name: 'authorName',
+            label: 'Author Username',
+            type: 'string',
+            contextual: true
+        }
+    ],
+
+    construct: function (self, options) {
+        self.beforeSave = function (req, piece, options, callback) {
+
+            console.log(req.user);
+
+
+            piece._author = req.user["_id"];
+            piece.authorName = req.user.username;
+
+            console.log(piece);
+
+
+            return callback();
+        };
     }
-  ],
 
-    beforeSave: function(self, options) {
-
-        console.log(self.toString());
-        console.log(options.toString());
-
-    }
-  
 };
